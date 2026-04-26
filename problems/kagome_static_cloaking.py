8a0babf8d0497567213c1763d6c351185bd20068
Giovanni Bordiga <gio.61192@gmail.com>
2026-04-26 16:21:16 -0400

    Refactor solution data handling in ForwardProblem class for improved flexibility with dict and list types

--- problems/kagome_static_cloaking.py
+++ problems/kagome_static_cloaking.py
@@ -548,11 +548,15 @@ class ForwardProblem:
         # Convert solution data to named tuple
         if dict_in["solution_data"] is not None:
             if type(dict_in["solution_data"]) is dict:
-                dict_in["solution_data"] = SolutionData(
-                    **dict_in["solution_data"])
+                dict_in["solution_data"] = {
+                    key: SolutionData(
+                        **solution) if type(solution) is not list else [SolutionData(**s) for s in solution]
+                    for key, solution in dict_in["solution_data"].items()
+                }
             elif type(dict_in["solution_data"]) is list:
-                dict_in["solution_data"] = [SolutionData(
-                    **solution) for solution in dict_in["solution_data"]]
+                dict_in["solution_data"] = [
+                    SolutionData(**solution) for solution in dict_in["solution_data"]
+                ]
         problem_data = ForwardProblem(**dict_in)
         problem_data.is_setup = False
         return problem_data
@@ -565,6 +569,11 @@ class ForwardProblem:
         elif type(dict_out["solution_data"]) is list:
             dict_out["solution_data"] = [solution._asdict()
                                          for solution in dict_out["solution_data"]]
+        elif type(dict_out["solution_data"]) is dict:
+            dict_out["solution_data"] = {
+                key: solution._asdict() if type(solution) is not list else [s._asdict() for s in solution]
+                for key, solution in dict_out["solution_data"].items()
+            }
         return dict_out
 
 
